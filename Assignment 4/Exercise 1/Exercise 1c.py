fe486ff28c3d1468e77857f72a6879ef82e6e41d
GwnElviin <0907984@hr.nl>
2015-09-26 17:14:31 +0200

    uploading files

---
+++ Assignment 4/Exercise 1/Exercise 1c.py
@@ -0,0 +1,17 @@
+__author__ = 'Elvin Carvalho, 0907984 INFB1'
+
+#function to calculate absolute
+def absoluteN():
+    q1 = input('Input number: ')
+    formula = abs(q1)
+    print('The absolute of %s = %s' %(q1,formula))
+
+#check if no input is given
+try:
+    absoluteN()
+except SyntaxError:
+    q1 = None
+    print('No input/Wrong input given')
+    absoluteN()
+
+
