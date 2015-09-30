88ac0ff7d86eb25801a17594f1e27599063a61d5
GwnElviin <0907984@hr.nl>
2015-09-30 10:16:53 +0200

    Changed #comment

---
+++ Assignment 5/exercise 1/Exercise 1a.py
@@ -0,0 +1,7 @@
+__author__ = 'Elvin Carvalho, 0907984 INFB1'
+
+#Ask for user Input
+q1 = raw_input('Input a string and i will reverse it.: \n')
+
+#[where string output should begin(left empty because i want the whole string):where the string should end(left empty because i want the whole string):steps == -1 so the string is reversed]
+print q1[::-1]
